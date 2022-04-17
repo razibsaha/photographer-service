a5ee9495268bb55b8ce76defba18b146ea567577
Razib Saha <rajibpay@gmail.com>
2022-04-17 23:31:44 +0600

    first commit

---
+++ src/Components/Shared/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../../Assets/Images/logo.png'
+
+const Header = () => {
+    return (
+        <div>
+            <img src={logo} alt="" />
+        </div>
+    );
+};
+
+export default Header;
